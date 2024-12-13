bfc67d7caccea43827e657ab13bb47fbd15268c0
Grove1212 <stella223@korea.ac.kr>
2024-12-13 23:01:03 +0900

    refactor: 유효성 검사를 하는 클래스 분리

--- src/main/kotlin/oncall/view/InputValidator.kt
+++ src/main/kotlin/oncall/view/InputValidator.kt
@@ -1,4 +1,22 @@
 package oncall.view
 
-class InputValidator {
+import camp.nextstep.edu.missionutils.Console
+
+ object InputValidator {
+    fun validateGetMonthAndStartDayOfTheWeek(input: List<String>) {
+        val month = input[0].toInt()
+        require(month in 1..12) {
+            "[ERROR] Invalid month: $month 다시 입력해 주세요."
+        }
+        val dayOfWeek = input[1]
+        val validDays = setOf("월", "화", "수", "목", "금", "토", "일")
+        require(dayOfWeek in validDays) { "[ERROR] Invalid dayOfWeek: $dayOfWeek 다시 입력해 주세요." }
+    }
+
+    fun validateGetOrder(input: List<String>) {
+        input.forEach { require(it.length <= 5) { "[ERROR] 닉네임 길이는 5자를 초과하지 않습니다. 다시 입력해 주세요." } }
+        require(input.size in 5..35) { "[ERROR] 순번은 5명에서 35명 사이여야 합니다." }
+        val inputMap = input.toSet()
+        require(input.size == inputMap.size) { "[ERROR] 이름 중복입니다. 다시 입력해주세요." }
+    }
 }
--- src/main/kotlin/oncall/view/InputView.kt
+++ src/main/kotlin/oncall/view/InputView.kt
@@ -19,15 +19,8 @@ class InputView {
     fun getMonthAndStartDayOfTheWeek(): Pair<Int, String> {
         println(MONTHANDSTARTDAYOFWEEK)
         val input = Console.readLine().split(",") ?: throw IllegalArgumentException("[ERROR] 입력을 받을 수 없습니다.")
-        val month = input[0].toInt()
-        require(month in 1..12) {
-            "[ERROR] Invalid month: $month 다시 입력해 주세요."
-        }
-        val dayOfWeek = input[1]
-        val validDays = setOf("월", "화", "수", "목", "금", "토", "일")
-        require(dayOfWeek in validDays) { "[ERROR] Invalid dayOfWeek: $dayOfWeek 다시 입력해 주세요." }
-
-        return Pair(month, dayOfWeek)
+        InputValidator.validateGetMonthAndStartDayOfTheWeek(input)
+        return Pair(input[0].toInt(), input[1])
     }
 
     fun getWeekdayOrder(): List<String> {
@@ -42,10 +35,7 @@ class InputView {
 
     private fun getOrder(): List<String> {
         val input = Console.readLine().split(",") ?: throw IllegalArgumentException("[ERROR] 입력을 받을 수 없습니다.")
-        input.forEach { require(it.length <= 5) { "[ERROR] 닉네임 길이는 5자를 초과하지 않습니다. 다시 입력해 주세요." } }
-        require(input.size in 5..35) { "[ERROR] 순번은 5명에서 35명 사이여야 합니다." }
-        val inputMap = input.toSet()
-        require(input.size == inputMap.size) { "[ERROR] 이름 중복입니다. 다시 입력해주세요." }
+        InputValidator.validateGetOrder(input)
         return input
     }
 
