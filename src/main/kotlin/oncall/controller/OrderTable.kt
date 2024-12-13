ac0dd240044381ad0eda0b2d0ff13042fc7c2440
Grove1212 <stella223@korea.ac.kr>
2024-12-13 20:27:17 +0900

    feat: Cell로 구성된 빈 근무표 생성

--- src/main/kotlin/oncall/controller/OrderTable.kt
+++ src/main/kotlin/oncall/controller/OrderTable.kt
@@ -1,5 +1,6 @@
 package oncall.controller
 
+import oncall.model.Cell
 import oncall.view.OutputView
 import java.time.DayOfWeek
 
@@ -12,21 +13,42 @@ class OrderTable(
         val weekdayOrder = inputController.controlGetWeekdayOrder()
         val holidayOrder = inputController.controlGetHolidayOrder()
 
+        val shiftTable = makeShiftTable(month, startWeekDay)
+        putWeekdayOrder(shiftTable)
+        putHolidayOrder(shiftTable)
+    }
 
+    private fun makeShiftTable(month: Int, startWeekDayOfString: String): MutableList<Cell> {
+        val shiftTable = mutableListOf<Cell>()
+        val lastDateOfMonth = getLastDateOfMonth(month)
+        val weekDay = getStartWeekDay(startWeekDayOfString)
+
+        for (i in 1..lastDateOfMonth) {
+            shiftTable.add(Cell(month, i, weekDay?.plus(1)))
+        }
+        return shiftTable
+    }
+
+    private fun putHolidayOrder(shiftTable: MutableList<Cell>) {
+        TODO("Not yet implemented")
+    }
+
+    private fun putWeekdayOrder(shiftTable: MutableList<Cell>) {
+        TODO("Not yet implemented")
     }
 
     fun getStartWeekDay(startWeekDay: String): DayOfWeek? = when (startWeekDay) {
-        "월" ->DayOfWeek.MONDAY
-        "화" ->DayOfWeek.SATURDAY
-        "수" ->DayOfWeek.TUESDAY
-        "목" ->DayOfWeek.THURSDAY
-        "금" ->DayOfWeek.FRIDAY
-        "토" ->DayOfWeek.SATURDAY
-        "일" ->DayOfWeek.SUNDAY
-        else ->null
+        "월" -> DayOfWeek.MONDAY
+        "화" -> DayOfWeek.SATURDAY
+        "수" -> DayOfWeek.TUESDAY
+        "목" -> DayOfWeek.THURSDAY
+        "금" -> DayOfWeek.FRIDAY
+        "토" -> DayOfWeek.SATURDAY
+        "일" -> DayOfWeek.SUNDAY
+        else -> null
     }
 
-    fun getLastDayOfMonth(month: Int): Int = when (month) {
+    fun getLastDateOfMonth(month: Int): Int = when (month) {
         2 -> 28
         1, 3, 5, 7, 8, 10, 12 -> 31
         4, 6, 9, 11 -> 30
--- src/main/kotlin/oncall/model/Cell.kt
+++ src/main/kotlin/oncall/model/Cell.kt
@@ -1,4 +1,13 @@
 package oncall.model
 
-class Cell {
+import java.time.DayOfWeek
+
+class Cell(
+    val month: Int,
+    val date: Int,
+    val dayOfWeek: DayOfWeek?,
+    val employee: String = "",
+    val isHoliday: Boolean = false
+) {
+
 }
